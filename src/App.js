c9d0218ff68e40e158e4e0459764506ec502fd2e
sirihemsley <siri.hemsley@gmail.com>
2021-05-06 16:44:31 +0200

    removed one thing that failed the deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { RandomShow } from './components/RandomShow'
-import { FilterShows } from './components/FilterShows'
+// import { FilterShows } from './components/FilterShows'
 
 const Main = styled.main`
   background-color: #161a1d;
